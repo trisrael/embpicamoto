d019f60fbc2dfe7068cb1cd508857793bd92a843
Tristan Goffman <tgoffman@gmail.com>
2011-06-26 13:01:33 -0700

    switch function expession to variable instead

--- embpicamoto.php
+++ embpicamoto.php
@@ -125,7 +125,7 @@ function add_embpicamoto_shortcode($atts, $content = null) {
 					$script = '<script type=”text/javascript”>';					
 					$script = $script . "(function(){";
 					$script = $script . "var t = setTimeout('initTabs()', 500);"; #Attempt to setup tabs every 1/2 second
-					$script = $script . "function initTabs(){";
+					$script = $script . "var initTabs = function(){";
 					$script = $script . "if(jQuery && jQuery.ui && jQuery.ui.tabs){ clearTimeout(t); jQuery('#$wrap_el_id').tabs();}";
 					$script = $script . "}";
 					$script = $script . '})();</script>';
