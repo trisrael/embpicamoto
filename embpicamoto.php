90eb08a1ab9e345e2aedea02fe4e54b163f53736
Tristan Goffman <tgoffman@gmail.com>
2011-06-27 14:17:25 -0700

    fixed javascript syntax errors

--- embpicamoto.php
+++ embpicamoto.php
@@ -125,9 +125,10 @@ function add_embpicamoto_shortcode($atts, $content = null) {
 					$script = '<script type=\'text/javascript\'>';					
 					$script = $script . "(function(){";
 					$script = $script . "var timesTried=0;";
-					$script = $script . "var jLoaded = function(){return (typeof jQuery == 'function' && typeof jQuery.ui == 'object' && typeof jQuery.ui.tabs == 'function');}";
-					$script = $script . "var timed = function(){ setTimeout(function(){if(timesTried>20){return;} if(jLoaded()  ) { setTabs(); } else{ alert('not done still'); timesTried++;timed(); }  }, 500) };";
-					$script = $script . "timed();}){}";
+					$script = $script . "var setTabs = function(){ jQuery('#$wrap_el_id').tabs(); };";
+					$script = $script . "var jLoaded = function(){ return (typeof jQuery == 'function' && typeof jQuery.ui == 'object' && typeof jQuery.ui.tabs == 'function'); };";
+					$script = $script . "var timed = function(){ setTimeout( function(){if(timesTried>20){return;} if(jLoaded()){ setTabs(); }else{ timesTried++; timed(); }  }, 500) };";
+					$script = $script . "timed();})()";
 					$script = $script . "</script>";
 			
 					$html_page_names = '<ul>';
