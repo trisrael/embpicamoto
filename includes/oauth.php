3f79479a39a98500c422a75495fdda2b1c6d0d62
Tristan Goffman <tgoffman@gmail.com>
2011-07-05 01:01:52 -0700

    add a settings class in oauth utils to pull keys from Settings API as needed

--- includes/oauth.php
+++ includes/oauth.php
@@ -1,20 +1,22 @@
 <?php
 
 //Google Oauth
-namespace empicamoto\google {
-	
+namespace empicamoto\oauth\google {
+
+	use embpicamoto\oauth\util\Defaults;
+	use embpicamoto\oauth\util\Settings;
+
 	set_include_path ( implode ( PATH_SEPARATOR, array (realpath ( dirname ( __FILE__ ) . '/../library' ), get_include_path () ) ) );
 	require_once 'Zend/Loader.php';
 	require_once 'Zend/Oauth/Consumer.php';
 	require_once 'namespace_util.php';
 	require_once 'oauth_util.php';
-	//Zend_Loader::loadClass ( 'Zend_OAuth_Consumer' );
+	//Zend_Loader::loadClass ( 'Zend_OAuth_Consumer' );	
 	
-	use embpicamotoOAuth;
 	
 	interface AuthenticationUrls {
 		public function get_request_token_url();
-		public function get_request_callback_url();
+		public function get_request_callback_url();		
 	}
 
 	
@@ -42,13 +44,13 @@ namespace empicamoto\google {
 		
 		//Function testing whether user has changed their oauth consumer/secret from defaults
 		public function is_using_defaults() {
-			return get_consumer_key () == Defaults::consumerKey && get_consumer_secret () == Defaults::consumerSecret;
+			return Settings::get_consumer_key () == Defaults::consumerKey && Settings::get_consumer_secret () == Defaults::consumerSecret;
 		}
 		
 		//Test whether site has been authenticated correctly with Google services
 		public function has_valid_accreditation() {
 			if (! isset ( $cons )) {
-				$config = array ('callbackUrl' => get_request_callback_url (), 'siteUrl' => get_request_token_url (), 'consumerKey' => get_consumer_key (), 'consumerSecret' => get_consumer_secret () );
+				$config = array ('callbackUrl' => Settings::get_request_callback_url (), 'siteUrl' => Settings::get_consumer_key(), 'consumerKey' => get_consumer_key (), 'consumerSecret' => get_consumer_secret () );
 				$cons = new Zend_Oauth_Consumer ( $config );
 				// fetch a request token
 				$reqToken = $cons->getRequestToken ();
--- includes/oauth_util.php
+++ includes/oauth_util.php
@@ -1,40 +1,43 @@
-<?php namespace embpicamotoOAuth;
+<?php
+namespace embpicamoto\oauth\util;
 
-	//Register OAuth Settings
-	class Consts {		
-		//Re-used strings
-		const google = 'google';
+//Register OAuth Settings
+class Consts {
+	//Re-used strings
+	const google = 'google';
 	
-		//Wordpress ids/variable names
-		const SettingsId = 'embpicamoto_oauth_settings';
-		const GSectionId = 'google_oauth_section';		
-		const GSectionName = self::google;
-		const GConsumerPre = "embpicamoto_oauth_google_consumer_";		
+	//Wordpress ids/variable names
+	const SettingsId = 'embpicamoto_oauth_settings';
+	const GSectionId = 'google_oauth_section';
+	const GSectionName = self::google;
+	const GConsumerPre = "embpicamoto_oauth_google_consumer_";
 	
-		public static function consumerId($str){
-			return self::GConsumerPre . $str;
-		}
-		
-		const key = 'key';
-		
-		public static function consumerKeyId(){
-			self::consumerId(key);
-		}
-
-		const secret = 'secret';
-		public static function consumerSecretId(){
-			self::consumerId(secret);
-		}		
-	};	
-	
-	function get_consumer_key(){
-		get_option(Consts::consumerKeyId());
+	public static function consumerId($str) {
+		return self::GConsumerPre . $str;
 	}
 	
+	const key = 'key';
 	
-	function get_consumer_secret(){
-		get_option(Consts::consumerSecretId());
+	public static function consumerKeyId() {
+		self::consumerId ( key );
 	}
+	
+	const secret = 'secret';
+	public static function consumerSecretId() {
+		self::consumerId ( secret );
+	}
+}
+;
+
+class Settings {
+	static function get_consumer_key() {
+		get_option ( Consts::consumerKeyId () );
+	}
+	
+	static function get_consumer_secret() {
+		get_option ( Consts::consumerSecretId () );
+	}
+}
 
 class Defaults {
 	const consumerKey = '';
