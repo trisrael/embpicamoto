d2c4cce6736ff60cbd1b84eb60bc162ed319a5b7
Tristan Goffman <tgoffman@gmail.com>
2011-07-04 18:09:04 -0700

    including google oauth with interfaces plus abstract classes, attempting to get it all functioning together

--- includes/oauth.php
+++ includes/oauth.php
@@ -1,28 +1,27 @@
 <?php
 namespace embpicamoto {
 	use embpicamotoOAuth\Defaults;
-
+	
 	require_once 'namespace_util.php';
-	require_once 'oauth_util.php';	
-
-	interface OauthUrls{
-		public function get_request_token_url();		
+	require_once 'oauth_util.php';
+	
+	interface OauthUrls {
+		public function get_request_token_url();
 		public function get_request_callback_url();
 	}
 	
-	class AbstractOAuth implements OauthUrls {		
+	class AbstractOAuth implements OauthUrls {
 		
 		private static $instance;
 		//Zend consumer object
 		private $cons;
-
-		private $reqToken;
 		
+		private $reqToken;
 		
 		//A private constructor; prevents direct creation of object
 		private function __construct() {
-		}		 		
-
+		}
+		
 		// The singleton method
 		public static function singleton() {
 			if (! isset ( self::$instance )) {
@@ -41,48 +40,39 @@ namespace embpicamoto {
 		//Test whether site has been authenticated correctly with Google services
 		public function has_valid_accreditation() {
 			if (! isset ( $cons )) {
-				$config = array (
-					'callbackUrl' => get_request_callback_url (), 
-					'siteUrl' => get_request_token_url(), 
-					'consumerKey' => get_consumer_key(), 
-					'consumerSecret' => get_consumer_secret() 
-				);
+				$config = array ('callbackUrl' => get_request_callback_url (), 'siteUrl' => get_request_token_url (), 'consumerKey' => get_consumer_key (), 'consumerSecret' => get_consumer_secret () );
 				$cons = new Zend_Oauth_Consumer ( $config );
 				// fetch a request token
-				$reqToken = $cons->getRequestToken();
+				$reqToken = $cons->getRequestToken ();
 				
-				$reqToken->isValid();
+				$reqToken->isValid ();
 			}
-		}	
+		}
 	}
+}
+//Google Oauth
+namespace empicamoto\google {
+	/** Zend_Oauth_Consumer*/
+	use embpicamoto\AbstractOAuth as Abs;	
 	
-	//Google Oauth
-	namespace google {
-		/** Zend_Oauth_Consumer*/
-		use embpicamoto\AbstractOAuth as Abs;
-		use embpicamoto\get_callback_url;
+	require_once 'library/Zend/Oauth/Consumer.php';
+	use embpicamotoOAuth\get_consumer_secret;
+	use embpicamotoOAuth\get_consumer_key;
+	use embpicamotoOAuth\Defaults as Defaults;
+	
+	class OAuth extends Abs {
+		static $requestUrl = 'https://www.google.com/accounts/OAuthGetRequestToken';
 		
-		require_once 'library/Zend/Oauth/Consumer.php';
-		use embpicamotoOAuth\get_consumer_secret;
-		use embpicamotoOAuth\get_consumer_key;
-		use embpicamotoOAuth\Defaults as Defaults;
+		function get_request_token_url() {			
+			return self::$requestUrl;
+		}		
 		
-		class OAuth extends Abs{
-			static $requestUrl = 'https://www.google.com/accounts/OAuthGetRequestToken';		
-			
-			function get_request_token_url(){
-				
-				return self::siteUrl;
-			}
-			
-			function get_request_callback_url(){
-				return plugins_dir("request_callback.php", __FILE__);
-			}			
-			
-		}	
-		
+		function get_request_callback_url() {
+			return plugins_dir ( "request_callback.php", __FILE__ );
+		}
 	
 	}
 
 }
+
 ?>
