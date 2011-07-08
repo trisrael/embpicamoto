c0b22bf915b2b83557df3627693c6e63164afe79
Tristan Goffman <tristan@fotomoto.com>
2011-07-07 21:47:20 -0700

    Oauth implemented

--- embpicamoto.php
+++ embpicamoto.php
@@ -31,8 +31,7 @@ class Embpicamoto_Photos {
         $gauth = Embpicamoto_Oauth_Google_Manager::singleton();
 
         $client = null; #Should cause an interesting exception if neither works
-        if ($gauth->has_access_token()) {
-            throw new Exception("Using Oauth");
+        if ($gauth->has_access_token()) {            
             $client = $gauth->getAccessToken()->getHttpClient($gauth->getConfig());
         } else {
             $login_val = Embpicamoto_Settings_Helper::getLogin();
