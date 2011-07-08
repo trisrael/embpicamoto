d2e515ec8ee3546e3dd09b63b96d8d27276ad8f7
Tristan Goffman <tristan@fotomoto.com>
2011-07-08 11:09:03 -0700

    wrap with function to allow for $ with jquery

--- dialog_manager.js
+++ dialog_manager.js
@@ -1,27 +1,31 @@
-Embpicamoto = {Dialog: {}};
+(function(){
+    var $ = jQuery;
 
-Embpicamoto.Dialog.Manager = {
+    Embpicamoto = {Dialog: {}};
 
-    
-    init: function(){
-        //Hide each album to begin with
-        jQuery.each(this.getAlbumEls(), function(i, el){ 
-            jQuery(el).hide();
-        })
-        
-        //show first album      
-        
-        
-    },
-    
-    
-    /*
-     Return each picamoto album HMTL container found within dialog
-     */
-    getAlbumEls: function(){
-        return jQuery("#embpicamoto_dlg #albums_container .picamoto_album");
+    Embpicamoto.Dialog.Manager = {
+
+
+        init: function(){
+            //Hide each album to begin with
+            $.each(this.getAlbumEls(), function(i, el){ 
+                $(el).hide();
+            })
+
+            //show first album      
+
+
+        },
+
+
+        /*
+         Return each picamoto album HMTL container found within dialog
+         */
+        getAlbumEls: function(){
+            return $("#embpicamoto_dlg #albums_container .picamoto_album");
+        }
+
+
     }
-    
-    
-}
 
+})(jQuery)
